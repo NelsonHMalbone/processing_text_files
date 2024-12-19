e7f1290ed7d3bd7656d79b564e4350286d6125f7
Nelson Malbone <nhmalbone@icloud.com>
2024-12-19 17:37:21 -0500

    opening each file with a for loop and also sorted each file with the sorted builted in

--- tutorial/main.py
+++ tutorial/main.py
@@ -7,5 +7,13 @@ import glob
 # input is the text files
 # using glob library
 
-filepaths = glob.glob("files/*.txt") # also known as a pattern
+filepaths = sorted(glob.glob("files/*.txt")) # also known as a pattern
 # * is a filter if more then just txt files where in folder would not see them
+# use sort to place files in abc order
+
+# open each text file
+# list of something what to iteriate
+for filepath in filepaths:
+    with open(filepath, 'r') as file:
+        content = file.read()
+
