65960355ed6ad1c7c85d82ddaa1bd118b5d92104
Nelson Malbone <nhmalbone@icloud.com>
2024-12-19 18:24:07 -0500

    combined txt files into one file

--- project/main.py
+++ project/main.py
@@ -2,3 +2,11 @@
 # using the glob
 import glob
 
+with open('combined_files.txt', 'w') as outfile: # creating new file
+    for filename in sorted(glob.glob('files/*.txt')): # interating over each file
+        with open(filename) as infile: # opens each file to read
+            for line in infile:
+                outfile.write(line)
+            outfile.write('\n\n##################\n\n')
+
+
